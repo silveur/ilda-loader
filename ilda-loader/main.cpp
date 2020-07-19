e4b7d490ef57ced9436138763dcb29a38bb56fd8
silveur <silvere.letellier@gmail.com>
2020-07-19 20:30:06 +0800

    Replacing QT functions

--- ilda-loader/main.cpp
+++ ilda-loader/main.cpp
@@ -2,10 +2,15 @@
 Main file for the Ilda reader. Creating a Max object able to read and decode ILDA frames
  */
 
+#include <string>
+#include <vector>
 #include "ext.h"
 #include "ext_obex.h"
 #include "ext_path.h"
+#include "../lib/IldaFrame.h"
 
+
+using namespace std;
 
 void *ildareader_class;
 
@@ -13,7 +18,6 @@ void *ildareader_class;
 
 void ext_main(void *r)
 {
-
-
+  
 	return (0);
 }
