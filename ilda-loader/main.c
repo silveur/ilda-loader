52c29124523ddb3d56eee27c112724117ae6dfa0
silveur <silvere.letellier@gmail.com>
2020-07-19 19:01:38 +0800

    First commit with Max lib

---
+++ ilda-loader/main.c
@@ -0,0 +1,19 @@
+/**
+Main file for the Ilda reader. Creating a Max object able to read and decode ILDA frames
+ */
+
+#include "ext.h"
+#include "ext_obex.h"
+#include "ext_path.h"
+
+
+void *ildareader_class;
+
+
+
+void ext_main(void *r)
+{
+
+
+	return (0);
+}
